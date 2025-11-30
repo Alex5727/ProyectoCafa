ae87c61f95b2c97e0d159b058cb2aee8b2e26086
xCorick <corick_serrano12@hotmail.com>
2025-11-30 13:23:39 -0700

    a

---
+++ Data/Interfaces/IUsersService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProyectoEncriptacion.Models;
+
+namespace Data.Interfaces
+{
+    public interface IUsersService
+    {
+        public Task<UsuarioModel> GetUsers();
+    }
+}
---
+++ Data/Services/UsersService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Data.Interfaces;
+using ProyectoEncriptacion.Models;
+
+namespace Data.Services
+{
+    public class UsersService : IUsersService
+    {
+        public async Task<UsuarioModel> GetUsers()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
