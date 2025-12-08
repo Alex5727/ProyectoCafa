e71c015a0c745258e358d4a6a9767fb347f47101
Guillermova123 <99280113+Guillermova123@users.noreply.github.com>
2025-12-07 16:00:24 -0800

    Commit terminado

---
+++ ProyectoEncriptacion/Controllers/AccountController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProyectoEncriptacion.Models;
+
+namespace ProyectoEncriptacion.Controllers
+{
+    public class RegistroController : Controller
+    {
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View(new RegisterViewModel());
+        }
+
+        [HttpPost]
+        public IActionResult Register(RegisterViewModel model)
+        {
+            if (!ModelState.IsValid)
+                return View(model);
+
+            // Aquí podrías guardar el usuario en una BD
+            // Ejemplo:
+            // _userService.Create(model.Username, model.Email, model.Password);
+
+            ViewBag.Message = "Usuario registrado correctamente";
+            return View("Register");
+        }
+    }
+}
